70c0e89cc8ec363124062d129d95a40fb127c073
Anastasia Voevodova <n.voevodova@yandex.ru>
2023-02-04 19:22:16 +0300

    exaples010

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 11, 21, 31, 14, 53, 31, 17, 85 };
+
+int n = array.Length;
+int find = 31;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine($"индекс {index}");
+        break;
+    }
+    index++;
+}
